cdc0f12bce6f45dd81e85e1b56b8e86441182701
Shawn Wilson <shawnwilsontrpt@gmail.com>
2021-01-12 17:34:59 -0500

    README.md finished, project complete! :rocket:

---
+++ README.md
@@ -0,0 +1,12 @@
+# Word-of-the-day scraper using Python
+
+## Purpose
+This script navigates to www.merriam-webster.com and pulls only the pertinent info for today's current word, definitions, and fun fact - and displays them in the terminal.
+
+## Tools 
+- python
+- requests module
+- beautifulsoup module
+
+### Further development
+I'd like to automate running this script every morning.
---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+import urllib.request
+from datetime import date
+from bs4 import BeautifulSoup
+
+#date header
+today = date.today()
+print("Word Of The Day for", today, "is: ")
+
+#url link
+url = 'https://www.merriam-webster.com/word-of-the-day'
+response = requests.get(url).text
+
+#word-of-the-day
+soup = BeautifulSoup(response, 'html.parser')
+wotd = soup.findAll('h1')[0]
+
+#class
+word_class = soup.find("span", class_='main-attr')
+
+#definitions
+definitions = soup.select("div.wod-definition-container > p")
+
+
+#did-you-know?
+dyk = soup.find("div", class_='left-content-box')
+
+
+print(wotd.text.upper(), "-", word_class.text)
+print()
+for el in definitions:
+    print(el.text)
+print()
+print(dyk.text)
+
